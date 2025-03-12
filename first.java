b548c398e2d9b2c5a2622fd7cf15f42c8cd1e672
manul <manulgupta1009@gmail.com>
2025-03-12 23:24:13 +0530

    lecture 4

--- first.java
+++ first.java
@@ -1,6 +1,92 @@
-class Firstclass {
-    public static void main (String args[]) {
-        System.out.println("Hello worlds!");
-        System.out.print("hello world");
-    }
+import java.util.*;
+
+// class Firstclass {
+
+//OUTPUT
+    // public static void main (String args[]) {
+    //     System.out.println("Hello worlds!");
+    //     System.out.print("hello world");
+    // }
+
+
+//INPUT
+    // public static void main(String args[]){
+        
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+    //     int sum = a+b ;
+    //     System.out.print(sum);
+
+    // }
+// }
+
+// -------------------------------------------------------------------------CONDITIONAL STATEMENTS --------------------------------
+
+
+// class Firstclass {
+//     public static void main (String args[]){
+//         Scanner sc = new Scanner(System.in);
+
+
+
+//     //     int age = sc.nextInt();
+
+//     //     if (age>=18) {
+//     //         System.out.println("Candidate can vote");
+            
+//     // } else {
+//     //     System.out.println("Candidate cant vote");
+//     // }
+
+
+//     int button = sc.nextInt();
+
+//     switch (button) {
+//         case 1:
+//             System.out.println("Hello!");
+//             break;
+
+//         case 2:
+//             System.out.println("Namaste!");
+//             break;
+        
+//         case 3:
+//              System.out.println("Bonjour!");     
+//              break;
+//         default:
+//             System.out.println("Invalid Button");
+//             break;
+//     }
+//     }
+// }
+
+
+//-----------------------------------------------------------------------------LOOPS---------------------------------------------------
+
+
+public class first {
+
+     public static void main (String args[]){
+          
+
+        // ----------------------------------------------------------------for---------------------------------------------
+        // for(int i = 1 ; i <= 10 ; i++){
+        //     System.out.println(i);
+        // }
+
+
+
+        //---------------------------------------------------------------while-------------------------------------------
+    
+    
+    //   int i=1;
+    //   int sum = 0;
+    //     while (i <= 10) {
+            
+    //         sum = sum + i;
+    //         i++;
+    //     }
+    //     System.out.println(sum);
+    //  }
 }
